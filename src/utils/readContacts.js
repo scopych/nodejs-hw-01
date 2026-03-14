7be025c1e536bfcda6bb15ac5305db9de529284d
scopych <scopych@gmail.com>
2026-03-14 18:08:14 +0200

    Implement step 1 and 2 of the homework.

--- src/utils/readContacts.js
+++ src/utils/readContacts.js
@@ -1,3 +1,13 @@
 import { PATH_DB } from '../constants/contacts.js';
+import { readFile } from 'node:fs/promises';
 
-export const readContacts = async () => {};
+export const readContacts = async () => {
+	try {
+		const  data = await readFile(PATH_DB, 'utf8');
+		return data;
+	} catch (error) {
+		console.error('there was an error:', error.message);
+	}
+}
+
+readContacts();
--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,3 +1,13 @@
 import { PATH_DB } from '../constants/contacts.js';
+import { writeFile } from 'node:fs/promises';
 
-export const writeContacts = async (updatedContacts) => {};
+export const writeContacts = async (updatedContacts) => {
+	try {
+		await writeFile(PATH_DB, updatedContacts, 'utf8');
+		console.log('Дані успішно записані у файл.');
+	} catch (err) {
+  // When a request is aborted - err is an AbortError
+  	console.error(err);
+	}	
+};
+
